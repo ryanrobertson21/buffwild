12a56e7336c7f8b82bf9bef94f497250e6496e53
Ryan Robertson <rtrobertson21@gmail.com>
2022-03-09 07:35:37 +0000

    added pythonanwhere allowed host

--- buffwild/settings.py
+++ buffwild/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "default_value")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['RyanRobertson21.pythonanywhere.com']
 
 
 # Application definition
