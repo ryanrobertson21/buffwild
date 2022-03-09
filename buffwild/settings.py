c0ad44b3f342efba854c2ce5d29463d4e832c96e
Ryan Robertson <rtrobertson21@gmail.com>
2022-03-08 20:29:40 -0800

    updated settings file and added requirements

--- buffwild/settings.py
+++ buffwild/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nb^d+s7zt3_kq4$+5gc!m%d@h#=8dwrlxq%qmp2y#iq$xpm+so'
+SECRET_KEY = os.environ.get('SECRET_KEY', "default_value")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
