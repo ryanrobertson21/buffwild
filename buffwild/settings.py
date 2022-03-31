0dda9b3e286bdd25e59ee4036fc909d424743591
Ryan Robertson <rtrobertson21@gmail.com>
2022-03-30 23:13:39 -0700

    messing around with static

--- buffwild/settings.py
+++ buffwild/settings.py
@@ -118,11 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+#STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')))
+#STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
