3522c2c3e3d1fba6f4ca30ffd114fcc860991190
Ryan Robertson <rtrobertson21@gmail.com>
2022-05-31 21:21:08 -0700

    pushing changes to add new db table for new 1 of 1s

--- main/forms.py
+++ main/forms.py
@@ -3,7 +3,7 @@ from django.core.validators import validate_image_file_extension
 from django.utils.translation import gettext as _
 
 
-from .models import Image
+from .models import Image, ImageTwo
 
 
 class ShowImageForm(forms.ModelForm):
@@ -29,4 +29,28 @@ class ShowImageForm(forms.ModelForm):
         """Process each uploaded image."""
         for upload in self.files.getlist("images"):
             image = Image(img_photo=upload)
+            image.save()
+
+class ShowImageFormTwo(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = (
+            "title",
+        )
+
+    images = forms.FileField(
+        widget=forms.ClearableFileInput(attrs={"multiple": True}),
+        label=_("Add photos"),
+        required=False,
+    )
+
+    def clean_photos(self):
+        """Make sure only images can be uploaded."""
+        for upload in self.files.getlist("images"):
+            validate_image_file_extension(upload)
+
+    def save_photos(self, show):
+        """Process each uploaded image."""
+        for upload in self.files.getlist("images"):
+            image = ImageTwo(img_photo=upload)
             image.save()
